6155c4a01a10bbbc5f0f7dd924c1b6a409cf7598
BookWorm916 <111474572+BookWorm916@users.noreply.github.com>
2024-03-23 14:39:34 -0500

    Add files via upload

--- filter-web-app-project113-116/main.js
+++ filter-web-app-project113-116/main.js
@@ -1,4 +1,8 @@
+noseX=0;
+noseY=0;
+
 function preload(){
+    mustache = loadImage("https://i.postimg.cc/3x3QzSGq/m.png")
 }
 
 function setup(){
@@ -12,12 +16,8 @@ function setup(){
     poseNet.on ('pose', gotPoses);
 }
 
-function modelLoaded(){
-    console.log('PoseNet is initialized')
-}
-
-function draw(){
-    image(video,0,0,300,300);
+function modelLoaded() {
+    console.log('PoseNet is Initialized')
 }
 
 function gotPoses(results)
@@ -25,9 +25,16 @@ function gotPoses(results)
     if(results.length > 0)
     {
         console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
         console.log("nose x = "+ results[0].pose.nose.x)
         console.log("nose y = "+ results[0].pose.nose.y)
     }
+}
+
+function draw(){
+    image(video,0,0,300,300);
+    image(mustache,noseX,noseY, 40,40)
 }
 
 function take_snapshot(){
